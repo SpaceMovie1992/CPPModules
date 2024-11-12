b1f595a0f7a4f022b2b0c44725325809339fbb36
SpaceMovie1992 <anahusic8@gmail.com>
2024-11-12 22:21:49 +0100

    ex00 pretty straight forward, did the makefile and header file first class Fixed easy to make. After that did the Fixed.cpp and main function. Everything compiles as it should there are no errors.

---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahusic <ahusic@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 22:20:44 by ahusic            #+#    #+#             */
+/*   Updated: 2024/11/12 22:20:46 by ahusic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
